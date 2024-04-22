e3f5de005634e480ea73a148262338de910ee67c
Oscar <osm1019@gmail.com>
2024-04-21 18:15:05 -0700

    test
    Change-Id: Ia0acaf675aa2b4e7ef2c7028b0ebf1d42395021a

--- config/common_full.mk
+++ config/common_full.mk
@@ -9,11 +9,13 @@ $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
 
 # Apps
 PRODUCT_PACKAGES += \
-    Eleven \
-    Etar \
-    Profiles \
-    Recorder \
-    Seedvault
+       OmniJaws \
+
+   # Eleven \
+      # Etar \
+   # Profiles \
+   # Recorder \
+   # Seedvault
 
 ifneq ($(PRODUCT_NO_CAMERA),true)
 PRODUCT_PACKAGES += \
