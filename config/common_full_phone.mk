1c11f136dfe63a912c62129fac0c199fb0306998
slogar25 <michaelslogar@gmail.com>
2025-05-10 00:13:42 +0000

    switch gms packages

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -8,13 +8,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 $(call inherit-product, vendor/lineage/config/telephony.mk)
 
 # GMS
-WITH_GMS ?= true
 ifeq ($(WITH_GMS),true)
-ifeq ($(TARGET_USES_MINI_GAPPS),true)
-$(call inherit-product, vendor/gms/gms_mini.mk)
-else ifeq ($(TARGET_USES_PICO_GAPPS),true)
-$(call inherit-product, vendor/gms/gms_pico.mk)
-else
-$(call inherit-product, vendor/gms/gms_full.mk)
-endif
+include vendor/gms/products/gms.mk
 endif
